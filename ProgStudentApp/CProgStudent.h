a25377cd8daea9112bd8fb655826157649daa662
adam nix <adamnix2004@gmail.com>
2024-10-10 17:28:57 +0100

    Refactor CProgStudent class and update main function
    Removed #include directives and using namespace std; from CProgStudent.cpp and added them to ProgStudentApp.cpp. Modified CProgStudent class in CProgStudent.h by removing and adding private members, and adding public constructors and methods. Updated main function in ProgStudentApp.cpp to handle CProgStudent objects, including user input and displaying student details. Removed main function from CProgStudent.cpp and added class methods.

--- ProgStudentApp/CProgStudent.h
+++ ProgStudentApp/CProgStudent.h
@@ -1,13 +1,26 @@
-#include <iostream>
-using namespace std;
 class CProgStudent
 {
-
 private:
 	string m_Name;
-	int m_WorkNo;
-	double m_HourlyRate;
-	int m_HoursWorked;
+	string m_KNum;
+	int m_ChristmasMark;
+	int m_LLAttendanceMark;
+	int m_Proj1Mark;
+	int m_Proj2Mark;
+	int m_MayMark;
 
+public:
+
+	CProgStudent(void);
+	CProgStudent(string name, string kNumber);
+
+	void SetMayExamMark(int mark);
+	void SetChristmasMark(int mark);
+	void SetProj1Mark(int mark);
+	void SetProj2Mark(int mark);
+	void SetLLAttendanceMark(int mark);
+
+	string GetName(void);
+	void ShowDetails(void);
+	int GetTotalMark(void);
 };
-
--- ProgStudentApp/ProgStudentApp.cpp
+++ ProgStudentApp/ProgStudentApp.cpp
@@ -1,11 +1,43 @@
-// ProgStudentApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
-using namespace std;
-#include <string>
-#include "CProgStudent.h"
+
+#include "CProgStudent.h" 
+
 int main()
 {
 
-}
+	CProgStudent list[3];
+
+	string name, knumber;
+
+
+	//Get names and work-numbers from the user
+	//Then instantiate an object and insert in the list
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Enter The neame anf K-number: ";
+		cin >> name >> workNo;
+		CProgStudent student(name, workNo);
+		list[i] = student;
+	}
+
+
+
+
+	//Display the details of each worker in the list
+	cout << "\n\n\n";
+	cout << "\t\tWorker Details\n";
+	for (int i = 0; i < 3; i++)
+	{
+		list[i].ShowDetails();
+	}
+
+	int mark; 
+
+	cout << "Enter Xmas mark for eatch student: ";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Enter Xmas mark for " << list[i].GetName() << " : ";
+		cin >> maark;
+		list[i].SetChristmasMark(mark);
+
+	}
