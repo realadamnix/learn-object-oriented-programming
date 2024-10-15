92103e75d213f7d1acb525bf3d59340e5a20d0ca
Adam Nix <150264616+realadamnix@users.noreply.github.com>
2024-10-15 08:41:40 +0100

    Changed solution files

--- WorkerAppV0/CWorker.cpp
+++ WorkerAppV0/CWorker.cpp
@@ -3,96 +3,96 @@
 //constructor function definitions
 CWorker::CWorker(void)
 {
-    m_Name = "";
-    m_WorkNo = 0;
-    m_HourlyRate = 0;
-    m_HoursWorked = 0;
+	m_Name = "";
+	m_WorkNo = 0;
+	m_HourlyRate = 0;
+	m_HoursWorked = 0;
 }
 
-//fred      5023
+//fred      5023  //8787
 CWorker::CWorker(string name, int workNo)
 {
-    m_Name = name;
-    m_WorkNo = workNo;
-    m_HourlyRate = 0;
-    m_HoursWorked = 0;
+	m_Name = name;
+	m_WorkNo = workNo;
+	m_HourlyRate = 0;
+	m_HoursWorked = 0;
 }
 
 
 //Regular function definitions
 
-                              //   12.50
+							  //   12.50
 void CWorker::SetHourlyRate(double rate)
 {
-    m_HourlyRate = rate;
+	m_HourlyRate = rate;
 }
 
 //10
 void CWorker::AddHours(int hours)
 {
-    m_HoursWorked += hours;
-    //m_HoursWorked = m_HoursWorked + hours;
+	m_HoursWorked += hours;
+	//m_HoursWorked = m_HoursWorked + hours;
 }
 
 
 double CWorker::GetWage(void)
 {
-    double wage;
-    wage = m_HourlyRate * m_HoursWorked;
-    return wage;
+	double wage;
+	wage = m_HourlyRate * m_HoursWorked;
+	return wage;
 }
 
 
 void CWorker::Display(void)
 {
-    //output the content of the worker object
-    cout << "Worker Name: " << m_Name << endl;
-    cout << "Worker Number: " << m_WorkNo << endl;
-    cout << "Hourly Rate: " << m_HourlyRate << endl;
-    cout << "Hours Worked: " << m_HoursWorked << endl;
+	//output the content of the worker object
+	cout << "Worker Name: " << m_Name << endl;
+	cout << "Worker Number: " << m_WorkNo << endl;
+	cout << "Hourly Rate: " << m_HourlyRate << endl;
+	cout << "Hours Worked: " << m_HoursWorked << endl;
 }
 
 
 void CWorker::SetHoursWorked(int hours)
 {
-    m_HoursWorked = hours;
+	m_HoursWorked = hours;
 }
 
 
 int CWorker::GetWorkNum(void)
 {
-    return m_WorkNo;
+	return m_WorkNo;
 }
 
 
 string CWorker::GetName(void)
 {
-    return m_Name;
+	return m_Name;
 }
 
 
 int CWorker::GetHoursWorked(void)
 {
-    return m_HoursWorked;
+	return m_HoursWorked;
 }
 
 
 double CWorker::GetHourlyRate(void)
 {
-    return m_HourlyRate;
+	return m_HourlyRate;
 }
 
 //1
 bool CWorker::HasWorkNum(int workNum)
 {
-    if (m_WorkNo == workNum)
-        return true;
-    return false;
+	if (m_WorkNo == workNum)
+		return true;
+	return false;
 }
 
 //50
 void CWorker::IncreaseRate(double percentIncrease)
 {
-    m_HourlyRate += m_HourlyRate * percentIncrease / 100;
+	m_HourlyRate += m_HourlyRate * percentIncrease / 100;
 
 }
